c09d5d0af8b0625bb416f46b09fffbf119a45ebc
buitahau <buitahau@gmail.com>
2023-07-28 11:29:43 +0700

    EFK deployment scripts and application

--- Readme.md
+++ Readme.md
@@ -15,3 +15,23 @@ https://refactorfirst.com/spring-boot-prometheus-grafana
 #### Fluentd
 
 https://arnoldgalovics.com/java-multiline-logs-fluentd/
+
+---
+
+### Deploy
+
+#### EFK (Elasticsearch - Fluentd - Kibana)
+
+``kubectl apply -f k8s/efk-stack.yaml``
+
+``kubectl -n kube-logging get services``
+
+To get the external IP, tunnel minikube ``minikube tunnel``, get the external IP, then go to Kibana UI with port 5601
+
+#### Spring java
+
+In Local ``eval $(minikube docker-env)`` first.
+
+``docker build -t fluentd-app:latest .``
+
+``kubectl apply -f k8s/fluentd-java.yaml``
--- src/main/java/owt/training/fluentd/FluentdApplication.java
+++ src/main/java/owt/training/fluentd/FluentdApplication.java
@@ -1,13 +1,34 @@
 package owt.training.fluentd;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
+@EnableScheduling
 public class FluentdApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(FluentdApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(FluentdApplication.class, args);
 	}
 
+	@Scheduled(fixedRate = 5000)
+	public void log() {
+		log.info("This is \\na multiline\\n\\n\\nlog");
+	}
+
+	@Scheduled(fixedRate = 10000)
+	public void logException() {
+		try {
+			String a = null;
+			log.info(a.toLowerCase());
+		} catch (NullPointerException e) {
+			throw new RuntimeException("Error happened", e);
+		}
+	}
 }
