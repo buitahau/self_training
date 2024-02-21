6ff8f64ee45d8d49f2f3789dda69d996d7dbcf36
haubui <buitahau@gmail.com>
2024-02-21 09:45:25 +0700

    feat: Initial project

---
+++ src/main/java/com/example/springbatch/SpringBatchApplication.java
@@ -0,0 +1,18 @@
+package com.example.springbatch;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Slf4j
+@EnableScheduling
+@EnableBatchProcessing
+@SpringBootApplication
+public class SpringBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchApplication.class, args);
+    }
+}
